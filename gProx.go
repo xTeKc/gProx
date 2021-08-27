f15210519fa58a5067a5f7a73ef601e96853acf1
xTeKc <bcd3v@pm.me>
2021-08-26 22:23:18 -0400

    create foowriter struct

--- gProx.go
+++ gProx.go
@@ -6,11 +6,14 @@ import (
 	"os"
 )
 
-//FooReader defines an io.Reader to read from stdin
+//FooReader defines an io.Reader to read from Stdin
 type FooReader struct{}
 
-//<Read> reads data from stdin
+//<Read> reads data from Stdin
 func (fooReader *FooReader) Read(b []byte) (int, error) {
 	fmt.Print("in > ")
 	return os.Stdin.Read(b)
 }
+
+//FooWriter defines an io.Writer to write to Stdout
+type FooWriter struct{}
