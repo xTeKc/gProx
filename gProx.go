f7ba6c0a9e4b20e7201e2f26bc836b75a66df2fb
xTeKc <bcd3v@pm.me>
2021-08-26 22:22:01 -0400

    read data

--- gProx.go
+++ gProx.go
@@ -6,5 +6,11 @@ import (
 	"os"
 )
 
-//defines an io.Reader to read from stdin
+//FooReader defines an io.Reader to read from stdin
 type FooReader struct{}
+
+//<Read> reads data from stdin
+func (fooReader *FooReader) Read(b []byte) (int, error) {
+	fmt.Print("in > ")
+	return os.Stdin.Read(b)
+}
